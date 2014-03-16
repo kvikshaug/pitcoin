f6096246e52e992d5ad9ca055955f52e5ae6d3f0
Ali Kaafarani <ali@kvikshaug.no>
2014-03-16 13:33:37 +0100

    Parse the script into chunks

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
+import os
+from io import BytesIO
+
 from protocoin.clients import BitcoinClient
 from protocoin.datatypes import messages, structures, values
 
 from db.models import Block
+from script_opcodes import *
 
 def run():
     client = TestClient("as")
@@ -13,26 +17,56 @@ class TestClient(BitcoinClient):
         super(TestClient, self).__init__(coin='bitcoin_testnet3', *args, **kwargs)
 
     def on_handshake(self):
-        print("Asking for: %s" % int(Block.objects.get(height=0).calculate_hash(), 16))
         self.send_message(messages.GetData(inventory=[structures.Inventory(
             inv_type=values.INVENTORY_TYPE["MSG_BLOCK"],
             inv_hash=int(Block.objects.get(height=0).calculate_hash(), 16),
         )]))
 
     def handle_block(self, header, block_message):
-        print("Got block: %s" % block_message)
+        print("Got genesis block: %s" % block_message)
         for tx in block_message.txns:
-            for tx_in in tx.tx_in:
-                script = Script(tx_in.signature_script)
+            for tx_out in tx.tx_out:
+                print(tx_out.pk_script)
+                script = Script(BytesIO(tx_out.pk_script))
 
 class Script(object):
-    def __init__(self, signature_script):
-        self.signature_script = signature_script
+    """The stack-based bitcoin script language for transaction redemption. See https://en.bitcoin.it/wiki/Script"""
+    def __init__(self, script):
         self.datastack = []
         self.ifstack = []
+        self.chunks = []
+        self.parse(script)
 
-    def parse(signature_script):
-        pass
+    def parse(self, script):
+        """Push data and opcodes into chunks"""
+
+        def available(stream):
+            current = stream.tell()
+            available = stream.seek(0, os.SEEK_END)
+            stream.seek(current, os.SEEK_SET)
+            return available
+
+        script_length = available(script)
+
+        while script.tell() < script_length:
+            opcode = int.from_bytes(script.read(1), byteorder='little')
+
+            if opcode >= 0 and opcode < OP_PUSHDATA1:
+                read_length = opcode
+                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+            elif opcode == OP_PUSHDATA1:
+                read_length = int.from_bytes(script.read(1), byteorder='little')
+                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+            elif opcode == OP_PUSHDATA2:
+                read_length = int.from_bytes(script.read(2), byteorder='little')
+                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+            elif opcode == OP_PUSHDATA4:
+                # OP_PUSHDATA4 should never be used, as pushes over 520 bytes are not allowed, and
+                # those below can be done using OP_PUSHDATA2, but we'll implement it nevertheless
+                read_length = int.from_bytes(script.read(4), byteorder='little')
+                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+            else:
+                self.chunks.append({'type': 'opcode', 'value': opcode})
 
     def run():
         pass
