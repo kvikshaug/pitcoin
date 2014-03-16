e5c2f96c022f0f017d684083239dec7eb1000416
Ali Kaafarani <ali@kvikshaug.no>
2014-03-16 13:38:40 +0100

    Don't read stream, just parse the bytearray

--- script.py
+++ script.py
@@ -1,6 +1,3 @@
-import os
-from io import BytesIO
-
 from protocoin.clients import BitcoinClient
 from protocoin.datatypes import messages, structures, values
 
@@ -27,10 +24,14 @@ class TestClient(BitcoinClient):
         for tx in block_message.txns:
             for tx_out in tx.tx_out:
                 print(tx_out.pk_script)
-                script = Script(BytesIO(tx_out.pk_script))
+                script = Script(tx_out.pk_script)
+                script.execute()
 
 class Script(object):
     """The stack-based bitcoin script language for transaction redemption. See https://en.bitcoin.it/wiki/Script"""
+
+    MAX_SCRIPT_DATA_SIZE = 520
+
     def __init__(self, script):
         self.datastack = []
         self.ifstack = []
@@ -38,35 +39,40 @@ class Script(object):
         self.parse(script)
 
     def parse(self, script):
-        """Push data and opcodes into chunks"""
+        """Parse pushed data and non-pushdata opcodes into chunks"""
 
-        def available(stream):
-            current = stream.tell()
-            available = stream.seek(0, os.SEEK_END)
-            stream.seek(current, os.SEEK_SET)
-            return available
-
-        script_length = available(script)
-
-        while script.tell() < script_length:
-            opcode = int.from_bytes(script.read(1), byteorder='little')
+        i = 0
+        while i < len(script):
+            opcode = script[i]
+            i += 1
 
             if opcode >= 0 and opcode < OP_PUSHDATA1:
                 read_length = opcode
-                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+                self.chunks.append({'type': 'data', 'value': script[i:i+read_length]})
+                i += read_length
             elif opcode == OP_PUSHDATA1:
                 read_length = int.from_bytes(script.read(1), byteorder='little')
-                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+                self.chunks.append({'type': 'data', 'value': script[i:i+read_length]})
+                i += read_length
             elif opcode == OP_PUSHDATA2:
                 read_length = int.from_bytes(script.read(2), byteorder='little')
-                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+                self.chunks.append({'type': 'data', 'value': script[i:i+read_length]})
+                i += read_length
             elif opcode == OP_PUSHDATA4:
                 # OP_PUSHDATA4 should never be used, as pushes over 520 bytes are not allowed, and
                 # those below can be done using OP_PUSHDATA2, but we'll implement it nevertheless
                 read_length = int.from_bytes(script.read(4), byteorder='little')
-                self.chunks.append({'type': 'data', 'value': script.read(read_length)})
+                self.chunks.append({'type': 'data', 'value': script[i:i+read_length]})
+                i += read_length
             else:
                 self.chunks.append({'type': 'opcode', 'value': opcode})
 
-    def run():
-        pass
+    def execute(self):
+        for chunk in self.chunks:
+            if chunk['type'] == 'data':
+                # Verify chunk length
+                if len(chunk['value']) > Script.MAX_SCRIPT_DATA_SIZE:
+                    raise ScriptException("Script pushed %s bytes of data, max is %s" % (len(chunk['value']), Script.MAX_SCRIPT_DATA_SIZE))
+
+class ScriptException(Exception):
+    """Thrown if the provided script has a syntax error or is otherwise invalid according to the Bitcoin Script rules."""
