ae2c3b05967aa9ad51c2b3cc6a2a2bd15c722e26
Ali Kaafarani <ali@kvikshaug.no>
2014-03-20 18:27:57 +0100

    Add has_length/include_length option for mpi converters

--- util/mpi.py
+++ util/mpi.py
@@ -2,27 +2,35 @@ import struct
 
 # Thanks to ArtForz from the bitcointalk forums for these snippets.
 # https://bitcointalk.org/index.php?topic=587.msg7482#msg7482
-# Slightly modified for python3.
+# Slightly modified for python3, and added has_length/include parameters
 # See http://www.openssl.org/docs/crypto/BN_bn2bin.html for more on the MPI format
 
-def mpi2num(m):
-    """convert MPI string to number"""
-    datasize = struct.unpack(">I", m[0:4])[0]
+def mpi2num(m, has_length=True):
+    """Convert MPI string to number. Set has_length to False if the values misses initial 4 length-bytes."""
+    if has_length:
+        datasize = struct.unpack(">I", m[0:4])[0]
+        data_start = 4
+    else:
+        datasize = len(m)
+        data_start = 0
     r = 0
     if datasize != 0:
-        neg_flag = bool(m[4] & 0x80)
-        r = m[4] & 0x7F
+        neg_flag = bool(m[data_start] & 0x80)
+        r = m[data_start] & 0x7F
         for i in range(1, datasize):
             r <<= 8
-            r += m[4+i]
+            r += m[(data_start) + i]
         if neg_flag:
             r = -r
     return r
 
-def num2mpi(n):
-    """convert number to MPI string"""
+def num2mpi(n, include_length=True):
+    """Convert number to MPI string. Set include_length to False to omit the initial 4 length-bytes."""
     if n == 0:
-        return struct.pack(">I", 0)
+        if include_length:
+            return struct.pack(">I", 0)
+        else:
+            return b''
     r = b""
     neg_flag = n < 0
     n = abs(n)
@@ -33,4 +41,6 @@ def num2mpi(n):
         r = bytes([0]) + r
     if neg_flag:
         r = bytes([r[0] | 0x80]) + r[1:]
-    return struct.pack(">I", len(r)) + r
+    if include_length:
+        r = struct.pack(">I", len(r)) + r
+    return r
